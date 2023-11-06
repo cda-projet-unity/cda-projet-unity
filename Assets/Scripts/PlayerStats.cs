8919b2beeb11d4f5bedab3b9b9e0b70884917f98
CyrilPonsan <cyril.ponsan@gmail.com>
2023-11-06 16:31:23 +0100

    update score

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class PlayerStats : MonoBehaviour
 {
     public static PlayerStats playerStats;
-    public bool gameOver;
 
     [SerializeField] private TextMeshProUGUI scoreTextComponent;
 
-    private int score;
+    private int score = 0;
 
     private void Awake()
     {
@@ -28,18 +28,27 @@ public class PlayerStats : MonoBehaviour
 
     private void Start()
     {
-        score = 0;
+        Debug.Log("hello start");
     }
+
 
     public void UpdateScore(int addScore)
     {
-        Debug.Log("updated");
+
         score += addScore;
+        if (scoreTextComponent == null)
+        {
+
+            Debug.Log("score text compo = null");
+            scoreTextComponent = gameObject.AddComponent<TextMeshProUGUI>();
+        }
         scoreTextComponent.text = "Score : " + score.ToString();
+
     }
 
     public void ResetScore()
     {
         Debug.Log("reseted");
+        score = 0;
     }
 }
