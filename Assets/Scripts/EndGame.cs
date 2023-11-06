cca2955c1cf381e466d6aa3c5d011a1fee67edd9
CyrilPonsan <cyril.ponsan@gmail.com>
2023-11-06 11:05:33 +0100

    end manager

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EndGame : MonoBehaviour
 {
+    public static EndGame endManager;
+    public bool gameOver;
+
+    private TextMeshProUGUI scoreTextComponent;
+
+    private int score;
+
     private void Awake()
     {
         if (endManager == null)
