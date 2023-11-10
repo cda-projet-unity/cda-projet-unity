f62cc1a420639080c04af4836724005cb0a3bb95
leo <mxmleosallier@gmail.com>
2023-11-10 13:54:08 +0100

    lapinou fix

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,37 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] private float aggroRange;
-    [SerializeField] protected float speed = 2f;
-    [SerializeField] protected int scoreAmount;
-    private bool scoreUpdated = false;
+    [SerializeField] public float aggroRange;
+    [SerializeField] public float speed = 2f;
+    [SerializeField] public int scoreAmount;
+    public bool scoreUpdated = false;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        foreach (ContactPoint2D hitPos in collision.contacts)
-        {
-            if (hitPos.normal.y < 0)
-            {
-                speed = 0;
-                Death();
-            }
-        }
-    }
 
-    public void Death()
-    {
-        Debug.Log("Aaaargh !");
-        if (!scoreUpdated)
-        {
-            PlayerStats.playerStats.UpdateScore(scoreAmount);
-            scoreUpdated = true;
-        }
-        StartCoroutine(DeathSequence());
-    }
-
-    IEnumerator DeathSequence()
-    {
-        yield return new WaitForSeconds(1f);
-        Destroy(gameObject);
-    }
 }
